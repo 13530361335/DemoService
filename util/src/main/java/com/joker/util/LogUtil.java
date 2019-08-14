94ade984b228a5c15bd622d7467ea270362a0013
Joker Jing <761878367@qq.com>
2019-08-15 01:42:23 +0800

    LogUtil

--- util/src/main/java/com/joker/util/LogUtil.java
+++ util/src/main/java/com/joker/util/LogUtil.java
@@ -2,11 +2,11 @@ package com.joker.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -46,15 +46,19 @@ public class LogUtil {
     private void log(String message, String level) {
         // 日志格式
         String pattern = "{0}  {1}  {2}";
-        String dateTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+        String dateTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd hh:mm:ss");
         String line = MessageFormat.format(pattern, dateTime, level, message);
-
         // FileUtils工具写入日志，日志路径自动创建
         try {
             FileUtils.writeLines(logFile, "utf-8", Arrays.asList(line), true);
         } catch (IOException e) {
             log.info(e.getMessage(), e);
         }
+    }
+
+    public static void main(String[] args) {
+        String format = DateFormatUtils.format(new Date(), "yyyy-MM-dd hh:mm:ss");
+        System.out.println(format);
     }
 
 }
